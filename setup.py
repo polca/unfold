e2b211f26d2b3a7b9e2a06c44832ec5f98155029
romainsacchi <r_s@me.com>
2024-12-07 15:19:30 +0100

    Version bump

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ def package_files(directory):
 
 setup(
     name="unfold",
-    version="1.2.2",
+    version="1.2.3",
     python_requires=">=3.10",
     packages=packages,
     author="Romain Sacchi <romain.sacchi@psi.ch>",
--- unfold/__init__.py
+++ unfold/__init__.py
@@ -1,5 +1,5 @@
 __all__ = ("Unfold", "Fold", "clear_cache")
-__version__ = (1, 2, 2)
+__version__ = (1, 2, 3)
 
 
 from .fold import Fold
